04fe1df966555dd15b6c4314912edea3dd9f5cf1
Marv_Tron <marvinatryonjr@gmail.com>
2019-03-25 14:19:15 -0700

    friends array/ html/ html routes

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+
+// Variable to hold the array of friends objects data
+var friends = [
+    { "name": "Kyle", "photo": "", "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] },
+    { "name": "Kate Burson", "photo": "", "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] }, 
+    { "name": "Matt Sainson", "photo": "", "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] }, 
+    { "name": "Lovdeep", "photo": "", "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] }, 
+    { "name": "Jason Cartwright", "photo": "", "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] }, 
+    { "name": "Marvin Tryon Jr", "photo": "", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] }];
+
+// Export the data
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 //console.log('hello world');
 
 // Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 // Sets up the Express App
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -17,7 +17,7 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT http://localhost" + PORT);
 });
 
 // Requires and sets the HTML routes in this file
